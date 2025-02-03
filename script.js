b156fa37a4079d02b42cb7b55fe1029095c9a290
Sai sasank Gunji <saisasnk.gunji@gmail.com>
2025-02-03 10:29:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,36 @@
-const counter=document.getElementById("count")
-const startBtn=document.getElementById("start")
-const endBtn=document.getElementById("stop")
-const resetBtn=document.getElementById("reset")
+const display=document.getElementById("display");
+let timer;
+let startTime=0;
+let elapsedTime=0;
+let isRunning=false;
 
-let intervalId;
-startBtn.onclick=function(){
-    intervalId=setInterval(()=>{
-        let count=Number(counter.value);
-        ++count;
-        counter.value=count;
-    },1000)
+document.getElementById("start").onclick=function(){
+    if(!isRunning){
+        currentTime=Date.now();
+        startTime=currentTime-elapsedTime;
+        timer=setInterval(update,10);
+        isRunning=true;
+    }
 }
-endBtn.onclick=function(){
-    clearInterval(intervalId);
+document.getElementById("stop").onclick=function(){
+    if(isRunning){
+        clearInterval(timer)
+        elapsedTime=Date.now()-startTime;
+        isRunning=false;
+    }
 }
-resetBtn.onclick=function(){
-    clearInterval(intervalId);
-    counter.value=0;
+document.getElementById("reset").onclick=function(){
+    clearInterval(timer)
+    isRunning=false
+    display.textContent="00:00:00:00"
+}
+function update(){
+    let currentTime=Date.now();
+    elapsedTime=currentTime-startTime;
+    let hrs=Math.floor(elapsedTime/(1000*60*60)).toString().padStart(2,0)
+    let mins=Math.floor(elapsedTime/(1000*60)%60).toString().padStart(2,0)
+    let secs=Math.floor(elapsedTime/(1000)%60).toString().padStart(2,0)
+    let ms=Math.floor((elapsedTime%1000)/10).toString().padStart(2,0)
+
+    display.textContent=`${hrs}:${mins}:${secs}:${ms}`;
 }
